1bcc4568758be678197ab46a98a10c2df83fe9e3
OrxanAbilov <abilov_orxan@hotmail.com>
2022-11-17 12:53:35 +0300

    Role Based Authorization

--- JWTAuthentication/Controllers/AuthController.cs
+++ JWTAuthentication/Controllers/AuthController.cs
@@ -53,7 +53,8 @@ namespace JWTAuthentication.Controllers
         {
             List<Claim> claims = new List<Claim>()
             {
-                new Claim(ClaimTypes.Name,user.UserName)
+                new Claim(ClaimTypes.Name,user.UserName),
+                new Claim(ClaimTypes.Role,"Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
